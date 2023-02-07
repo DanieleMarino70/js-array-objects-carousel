7c8303119cc3fb22635ccb7e4a8fbae52a69ab28
DanieleMarino70 <danielaotitan@gmail.com>
2023-02-07 18:38:50 +0100

    add: carousel structure

--- js/main.js
+++ js/main.js
@@ -33,12 +33,12 @@ const images = [
   {
     image: "./img/01.webp",
     title: "Marvel's Spiderman Miles Morale",
-    text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+    text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man."
   },
   {
     image: "./img/02.webp",
     title: "Ratchet & Clank: Rift Apart",
-    text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+    text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality."
   },
   {
     image: "./img/03.webp",
@@ -56,3 +56,54 @@ const images = [
     text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
   },
 ];
+
+
+// Milestone 1:
+// Ora rimuoviamo i contenuti statici e usiamo l'array di oggetti letterali per popolare dinamicamente il carosello.
+// Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+
+
+//PRENDIAMO GLI ELEMENTI CON ID "main-image" e "main-image-text"
+
+
+const mainContainerEl = document.getElementById("main-image");
+const mainImageTextEl = document.getElementById("main-image-text");
+const sliderEl = document.getElementById("slider-container");
+
+let index = 0;
+let counter = 0;
+
+for (const image of images){
+  
+  const imgMain = document.createElement("img");
+  const imgSlider = document.createElement("img");
+
+  const divEl = document.createElement("div");
+  const imgTitle = document.createElement("h2");
+  const imgText = document.createElement("p");
+
+  imgMain.src = image.image;
+  imgSlider.src = image.image;
+
+  imgTitle.innerHTML = image.title;
+  imgText.innerHTML = image.text;
+
+  if(index > 0){
+    imgMain.classList.add("hide");
+    imgTitle.classList.add("hide");
+    imgText.classList.add("hide");
+
+  }
+
+ mainContainerEl.append(imgMain);
+ sliderEl.append(imgSlider);
+ mainImageTextEl.append(divEl);
+ divEl.append(imgTitle, imgText);
+ index++;
+}
+
+
+
+// Milestone 2:
+// Aggiungere il **ciclo infinito** del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso sopra, 
+// la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sotto.
